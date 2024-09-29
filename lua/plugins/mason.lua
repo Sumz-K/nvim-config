1e82a95c77337c1f91bc9269e7abe9d5cc76f2f5
Sumz-K <sumukhkowndinya@gmail.com>
2024-09-29 16:43:53 +0530

    Add autocompletion

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -19,12 +19,23 @@ return
   {
     "neovim/nvim-lspconfig",
     config=function()
+      local capabilities=require('cmp_nvim_lsp').default_capabilities()
       local lspconfig=require('lspconfig')
-      lspconfig.clangd.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+      lspconfig.clangd.setup({
+        capabilities=capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities=capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilities=capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities=capabilities
+      })
+      lspconfig.ts_ls.setup({
+        capabilities=capabilities
+      })
       vim.keymap.set('n','K',vim.lsp.buf.hover,{})
       vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
       vim.keymap.set('n','<leader>rn',vim.lsp.buf.rename,{})
